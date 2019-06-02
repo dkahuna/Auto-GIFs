435dd33b6f771f23c76c883b9babb33e09976fc8
Khamhuang, David <khameye19888@gmail.com>
2019-06-02 00:31:42 -0500

    needs buttons to be displayed

---
+++ assets/gifTastic.js
@@ -0,0 +1,45 @@
+
+var topics = ["Toyota Supra", "Nissan Skyline", "Honda NSX", "Lexus RCF", "Subaru STI"];
+console.log (topics)
+function renderButtons() {
+
+$("#carGarage").empty();
+
+for (var i = 0; i < topics.length; i++) {
+
+    var gas = $("<button>");
+
+    gas.addClass("vehicle");
+    gas.attr("data-name", topics[i]);
+    gas.text(topics[i]);
+
+    $("#carGarage").append(gas);
+    }
+}
+
+$("#add-car").on("click", function(event) {
+event.preventDefault();
+var vehicle = $("#car-input").val().trim();
+topics.push(vehicle);
+renderButtons();
+
+});
+
+renderButtons();
+
+
+
+
+
+
+
+
+
+//var queryURL = "https://api.giphy.com/v1/gifs/trending?api_key=gIRGOQLe3uNe10Qngf230yvMSGeAt8l9";
+
+// $.ajax ({
+//     url: queryURL,
+//     method: "GET"
+// }).then(function(response) {
+//     console.log (response)
+// });
