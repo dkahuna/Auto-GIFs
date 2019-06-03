c80bc668c4dbb8a8395a7d3694af2205866eaba9
Khamhuang, David <khameye19888@gmail.com>
2019-06-02 20:54:33 -0500

    gifs and buttons

--- assets/gifTastic.js
+++ assets/gifTastic.js
@@ -1,43 +1,53 @@
-$(document).ready(function(){
-var topics = ["Toyota Supra", "Nissan Skyline", "Honda NSX", "Lexus RCF", "Subaru STI"];
+$(document).ready(function () {
+    var topics = ["Toyota Supra", "Nissan Skyline", "Honda NSX", "Lexus RCF", "Subaru STI"];
 
-function renderButtons() {
+    function renderButtons() {
 
-$("#carGarage").empty();
+        $("#carGarage").empty();
 
-for (var i = 0; i < topics.length; i++) {
-    var gas = $("<button>");
-    gas.addClass("vehicle");
-    gas.attr("data-name", topics[i]);
-    gas.text(topics[i]);
-    $("#carGarage").append(gas);
+        for (var i = 0; i < topics.length; i++) {
+            var gas = $("<button>");
+            gas.addClass("vehicle");
+            gas.attr("data-name", topics[i]);
+            gas.text(topics[i]);
+            $("#carGarage").append(gas);
+       
+        }
     }
+     
+    $("#add-car").on("click", function (event) {
+        event.preventDefault();
+        var model = $(this).attr("data-name");
+        var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
+            model + "&api_key=fdnhntrA0ytRHtfmdTFzNUoicyd1VT1i&limit=10";
+  
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        }).then(function (response) {
+            var results = response.data;
+            for (var i = 0; i < results.length; i++) {
+                var carDiv = $("<div>");
+                var p = $("<p>").text("Rating: " + results[i].rating);
+                var carImage = $("<img>");
+                carImage.attr("src", results[i].images.fixed_height.url);
+                carDiv.append(p);
+                carDiv.append(carImage);
+
+                $("#gifs-appear-here").prepend(carDiv);
+            }
+        });
+    });
+
+    renderButtons();
+});
+function newFunction_1(topics, renderButtons) {
+    newFunction(topics, renderButtons);
 }
 
-$("#add-car").on("click", function(event) {
-event.preventDefault();
-var vehicle = $("#car-input").val().trim();
-topics.push(vehicle);
-renderButtons();
+function newFunction(topics, renderButtons) {
+    var vehicle = $("#car-input").val().trim();
+    topics.push(vehicle);
+    renderButtons();
+}
 
-});
-
-renderButtons();
-});
-
-
-
-
-
-
-
-
-
-//var queryURL = "https://api.giphy.com/v1/gifs/trending?api_key=gIRGOQLe3uNe10Qngf230yvMSGeAt8l9";
-
-// $.ajax ({
-//     url: queryURL,
-//     method: "GET"
-// }).then(function(response) {
-//     console.log (response)
-// });
