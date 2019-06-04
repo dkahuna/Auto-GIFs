550406aceb550678a7162d63d3b0e020a576ad61
Khamhuang, David <khameye19888@gmail.com>
2019-06-04 16:19:31 -0500

    still no gifs with orginal buttons

--- assets/gifTastic.js
+++ assets/gifTastic.js
@@ -4,35 +4,35 @@ $(document).ready(function () {
     // function carShow() {
 
     //     var model = $(this).attr("data-name");
- 
-//$(this).attr("data-name");
+
+
     function renderButtons() {
-
-        $("#carGarage").empty();
-
+   $("#carGarage").empty();
         for (var i = 0; i < topics.length; i++) {
             var gas = $("<button>");
             gas.addClass("vehicle");
             gas.attr("data-name", topics[i]);
             gas.text(topics[i]);
             $("#carGarage").append(gas);
+            
         }
     }
 
     $("#add-car").on("click", function (event) {
         event.preventDefault();
-                 var vehicle = $("#car-input").val().trim();
+       
+        var vehicle = $("#car-input").val().trim();
          topics.push(vehicle);
         renderButtons();
-        var model = topics.length;
-        var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
+        var model = $(this).attr("carGarage");
+                 var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
             model + "&api_key=fdnhntrA0ytRHtfmdTFzNUoicyd1VT1i&limit=10";
-
         $.ajax({
             url: queryURL,
             method: "GET"
         }).then(function (response) {
             console.log(response)
+
             var results = response.data;
             for (var i = 0; i < results.length; i++) {
                 var carDiv = $("<div>");
