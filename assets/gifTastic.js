bf185235d448f5b8b4faf3e4d0a9c52e6aacc0e6
Khamhuang, David <khameye19888@gmail.com>
2019-06-05 20:54:54 -0500

    added some style to functioning app

--- assets/gifTastic.js
+++ assets/gifTastic.js
@@ -1,60 +1,26 @@
 $(document).ready(function () {
     var topics = ["Toyota Supra", "Nissan Skyline"];
-    
-    // function carShow() {
-
-    //     var model = $(this).attr("data-name");
 
 
     function renderButtons() {
-   $("#carGarage").empty();
+        $("#carGarage").empty();
         for (var i = 0; i < topics.length; i++) {
             var gas = $("<button>");
             gas.addClass("vehicle");
             gas.attr("data-name", topics[i]);
             gas.text(topics[i]);
             $("#carGarage").append(gas);
-            
+
         }
     }
 
-$("#carGarage").on("click",".vehicle", function(event){
-    
-    var trim = $(this).attr("data-name");
+    $("#carGarage").on("click", ".vehicle", function (event) {
 
-    
-             var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
-        trim + "&api_key=fdnhntrA0ytRHtfmdTFzNUoicyd1VT1i&limit=10";
-    $.ajax({
-        url: queryURL,
-        method: "GET"
-    }).then(function (response) {
-        console.log(response)
-
-        var results = response.data;
-        for (var i = 0; i < results.length; i++) {
-            var carDiv = $("<div>");
-            var p = $("<p>").text("Rating: " + results[i].rating);
-            var carImage = $("<img>");
-            carImage.attr("src", results[i].images.fixed_height.url);
-            carDiv.append(p);
-            carDiv.append(carImage);
-
-            $("#gifs-appear-here").prepend(carDiv);
-        }
-    });
-})
+        var trim = $(this).attr("data-name");
 
 
-    $("#add-car").on("click", function (event) {
-        event.preventDefault();
-  var  model = topics;
-        var vehicle = $("#car-input").val().trim();
-         topics.push(vehicle);
-        renderButtons();
-        
-                 var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
-            model + "&api_key=fdnhntrA0ytRHtfmdTFzNUoicyd1VT1i&limit=10";
+        var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
+            trim + "&api_key=fdnhntrA0ytRHtfmdTFzNUoicyd1VT1i&limit=10";
         $.ajax({
             url: queryURL,
             method: "GET"
@@ -66,16 +32,65 @@ $("#carGarage").on("click",".vehicle", function(event){
                 var carDiv = $("<div>");
                 var p = $("<p>").text("Rating: " + results[i].rating);
                 var carImage = $("<img>");
-                carImage.attr("src", results[i].images.fixed_height.url);
+                carImage.attr("src", results[i].images.fixed_height_still.url);
+                carImage.attr("data-still", results[i].images.fixed_height_still.url);
+                carImage.attr("data-animate", results[i].images.fixed_height.url);
+                carImage.addClass("wheels");
+                carImage.attr("state", "still");
                 carDiv.append(p);
                 carDiv.append(carImage);
 
                 $("#gifs-appear-here").prepend(carDiv);
             }
         });
+    })
+
+
+    $("#add-car").on("click", function (event) {
+        event.preventDefault();
+        var model = topics;
+        var vehicle = $("#car-input").val().trim();
+        topics.push(vehicle);
+        renderButtons();
+        
+
+// Commented these out to not have repeated gifs after a new button has been rendered.
+        
+        // var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
+        //     model + "&api_key=fdnhntrA0ytRHtfmdTFzNUoicyd1VT1i&limit=10";
+        // $.ajax({
+        //     url: queryURL,
+        //     method: "GET"
+        // }).then(function (response) {
+        //     console.log(response)
+
+        //     var results = response.data;
+        //     for (var i = 0; i < results.length; i++) {
+        //         var carDiv = $("<div>");
+        //         var p = $("<p>").text("Rating: " + results[i].rating);
+        //         var carImage = $("<img>");
+        //         carImage.attr("src", results[i].images.fixed_height.url);
+        //         carDiv.append(p);
+        //         carDiv.append(carImage);
+
+        //         $("#gifs-appear-here").prepend(carDiv);
+        //     }
+        // });
     });
 
+    $(document).on("click", ".wheels", function() {
+        var state = $(this).attr("data-state");      
+        if (state === "still") {
+          $(this).attr("src", $(this).attr("data-animate"));
+          $(this).attr("data-state", "animate");
+        } else {
+          $(this).attr("src", $(this).attr("data-still"));
+          $(this).attr("data-state", "still");
+        }
+      });
+
     renderButtons();
+    
 });
 
 
